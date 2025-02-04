521234dbc33a3a2f6c8906514f85f0867481b17d
Isaque Santos <paulopids38@gmail.com>
2025-02-04 18:19:55 -0300

    Alterei o estilo

--- calculo.js
+++ calculo.js
@@ -1,50 +1,50 @@
+// Função para calcular o IMC
 $('.calcular').click(function () {
+    // Obtendo elementos do DOM
+    const pesoElement = document.getElementById('peso');
+    const alturaElement = document.getElementById('altura');
+    const respostaElement = document.getElementById('resposta');
 
-    var pes = document.getElementById('peso')
-    var altur = document.getElementById('altura')
-    var resp = document.getElementById('resposta')
-    var p = Number(pes.value)
-    var alt = Number(altur.value)
-    var totimc = p / (alt * alt)
-    var imc = totimc.toFixed(2)
+    // Convertendo valores para números
+    const peso = Number(pesoElement.value);
+    const altura = Number(alturaElement.value);
 
-    if (p == "" || alt == "") {
-        alert('preencha todos os campos!')
-    } else {
-        if (imc <= 18.5) {
-            $('#resposta').css('display', 'flex');
-            $('#resposta').css('color', 'blue');
-            $('#resposta').html('Seu IMC é ' + imc + ', você está abaixo do peso!');
-            $('#resposta').prepend('<img src="img/abaixo-do-peso.jpg" width="60px">');
-        } if (imc >= 18.6 && imc <= 24.9) {
-            $('#resposta').css('display', 'flex');
-            $('#resposta').css('color', 'green');
-            $('#resposta').html('Seu IMC é ' + imc + ', você está com o peso ideal!');
-            $('#resposta').prepend('<img src="img/peso-normal.jpg" width="60px">');
-        } if (imc >= 25 && imc <= 29.9) {
-            $('#resposta').css('display', 'flex');
-            $('#resposta').css('color', 'yellow');
-            $('#resposta').html('Seu IMC é ' + imc + ', você está acima do peso!');
-            $('#resposta').prepend('<img src="img/acima-do-peso.jpg" width="60px">');
-        } if (imc >= 30 && imc <= 39.9) {
-            $('#resposta').css('display', 'flex');
-            $('#resposta').css('color', 'orange');
-            $('#resposta').html('Seu IMC é ' + imc + ', você está com Obesidade grau 1!');
-            $('#resposta').prepend('<img src="img/obesidade-grau1.jpg" width="60px">');
-        } if (imc > 40) {
-            $('#resposta').css('display', 'flex');
-            $('#resposta').css('color', 'red');
-            $('#resposta').html('Seu IMC é ' + imc + ', você está com Obesidade grau 2!');
-            $('#resposta').prepend('<img src="img/obesidade-grau2.jpg" width="60px">');
-        }
+    // Calculando o IMC
+    const imc = (peso / (altura * altura)).toFixed(2);
+
+    // Validação dos campos
+    if (!peso || !altura) {
+        alert('Por favor, preencha todos os campos!');
+        return;
     }
 
-})
+    // Configuração base da resposta
+    $('#resposta').css('display', 'flex');
 
+    // Definindo as categorias de IMC
+    if (imc <= 18.5) {
+        exibirResultado('blue', imc, 'abaixo do peso', 'abaixo-do-peso.jpg');
+    } else if (imc <= 24.9) {
+        exibirResultado('green', imc, 'com o peso ideal', 'peso-normal.jpg');
+    } else if (imc <= 29.9) {
+        exibirResultado('yellow', imc, 'acima do peso', 'acima-do-peso.jpg');
+    } else if (imc <= 39.9) {
+        exibirResultado('orange', imc, 'com Obesidade grau 1', 'obesidade-grau1.jpg');
+    } else {
+        exibirResultado('red', imc, 'com Obesidade grau 2', 'obesidade-grau2.jpg');
+    }
+});
+
+// Função auxiliar para exibir o resultado
+function exibirResultado(cor, imc, mensagem, imagem) {
+    $('#resposta')
+        .css('color', cor)
+        .html(`Seu IMC é ${imc}, você está ${mensagem}!`)
+        .prepend(`<img src="img/${imagem}" width="60px">`);
+}
+
+// Função para limpar os campos
 $('.limpar').click(function () {
-    $("#peso").val("")
-
-    $("#altura").val("")
-
-    $('#resposta').css('display', 'none')
-})
+    $("#peso, #altura").val("");
+    $('#resposta').css('display', 'none');
+});
